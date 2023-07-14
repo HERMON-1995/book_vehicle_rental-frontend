1f375725d07e5a1dba719e96a62899d05935eb16
shafiuyushawu <yishafiu@gmail.com>
2023-07-14 16:22:28 +0000

    Added harmburger icon

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,3 +1,4 @@
+import MenuIcon from '../components/MenuIcon';
 import SideBar from '../components/SideBar';
 
 const Reservations = () => (
@@ -5,7 +6,7 @@ const Reservations = () => (
     {/* eslint-disable jsx-a11y/label-has-associated-control */}
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content flex flex-col items-center justify-center">
-      <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+      <MenuIcon />
       {/* Page content here */}
       {/* <Reservation /> */}
       <h1 className="text-4xl">RESERVATIONS</h1>
--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -1,3 +1,4 @@
+import MenuIcon from '../components/MenuIcon';
 import SideBar from '../components/SideBar';
 
 const Vehicles = () => (
@@ -5,8 +6,7 @@ const Vehicles = () => (
     {/* eslint-disable jsx-a11y/label-has-associated-control */}
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content flex flex-col items-center justify-center">
-      <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-      {/* Page content here */}
+      <MenuIcon />
       {/* <Vehicles /> */}
       <h1 className="text-4xl">Vehicles</h1>
     </div>
