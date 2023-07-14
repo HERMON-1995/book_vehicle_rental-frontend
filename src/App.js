f608c03d1d89c1fb85f85263bef6494b24e18312
shafiuyushawu <yishafiu@gmail.com>
2023-07-14 15:51:33 +0000

    Implemented routing for various  pages.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import HomePage from './pages/HomePage';
 import ProtectedRoute from './pages/ProtectedRoute';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
+import Vehicles from './pages/Vehicles';
+import ReserveForm from './pages/ReserveForm';
+import Reservations from './pages/Reservations';
+import AddVehicle from './pages/AddVehicle';
+import RemoveVehicle from './pages/RemoveVehicle';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <ToastContainer />
       <BrowserRouter>
         <Routes>
@@ -21,6 +26,12 @@ function App() {
               </ProtectedRoute>
             )}
           />
+          <Route path="/vehicles" element={<Vehicles />} />
+          <Route path="/reseve-form" element={<ReserveForm />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/add-vehicle" element={<AddVehicle />} />
+          <Route path="/vehicles" element={<Vehicles />} />
+          <Route path="/remove-vehicle" element={<RemoveVehicle />} />
           <Route path="/register" element={<LoginPage />} />
           {/* <Route path="/*" element={<Error />} /> */}
         </Routes>
