f38c7456e8f0e89c90e885048da56f0f37161ae7
shafiuyushawu <yishafiu@gmail.com>
2023-07-14 15:52:08 +0000

    Implemented content page for home.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,15 @@
+import SideBar from '../components/SideBar';
+
 const HomePage = () => (
-  <div className="flex">
-    <h1 className="text-3xl font-bold underline text-red-5000">
-      Hello world!
-    </h1>
+  <div className="drawer lg:drawer-open">
+    {/* eslint-disable jsx-a11y/label-has-associated-control */}
+    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+    <div className="drawer-content flex flex-col items-center justify-center">
+      {/* Page content here */}
+      <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+    </div>
+    <SideBar />
   </div>
 );
 
