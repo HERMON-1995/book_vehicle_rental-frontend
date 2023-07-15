ea4cf03fa1c93bf12403f8da0ac714a5093f1713
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-15 18:14:56 +0300

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../slices/userSlice';
+import { carSlice } from '../slices/carSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    cars: carSlice.reducer,
   },
 });
 
