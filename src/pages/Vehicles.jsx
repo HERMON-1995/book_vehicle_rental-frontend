9ea44ea68b6ec34af6619e0dd0304ce2b43dead2
shafiuyushawu <yishafiu@gmail.com>
2023-07-16 23:10:05 +0000

    Used useEffect hook to fetch cars from the api to state.

--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -1,21 +1,31 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Slider from '../components/Slider';
 import MenuIcon from '../components/MenuIcon';
 import SideBar from '../components/SideBar';
+import { fetchCars } from '../redux/slices/carSlice';
 
-const Vehicles = () => (
-  <div className="flex">
-    <div className="lg:drawer-open">
-      {/* eslint-disable jsx-a11y/label-has-associated-control */}
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
-        <MenuIcon />
+const Vehicles = () => {
+  const { cars } = useSelector((state) => state.cars);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (cars.length === 0) dispatch(fetchCars());
+  }, [dispatch, cars.length]);
+  return (
+    <div className="flex">
+      <div className="lg:drawer-open">
+        {/* eslint-disable jsx-a11y/label-has-associated-control */}
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          <MenuIcon />
+        </div>
+        <SideBar />
       </div>
-      <SideBar />
+      <div className="sliderWrapper">
+        <Slider />
+      </div>
     </div>
-    <div className="sliderWrapper">
-      <Slider />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Vehicles;
