a2a499089955b4004d455871f8dc008922405e79
shafiuyushawu <yishafiu@gmail.com>
2023-07-23 22:00:31 +0000

    Created navbar for mobile version.

--- src/components/MenuIcon.jsx
+++ src/components/MenuIcon.jsx
@@ -3,7 +3,11 @@ import { AiOutlineMenuFold } from 'react-icons/ai';
 const MenuIcon = () => (
   <>
     {/* eslint-disable jsx-a11y/label-has-associated-control */}
-    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden"><AiOutlineMenuFold text-xl /></label>
+    <div className="navbar bg-base-100 shadow-xl mb-3 lg:hidden">
+      <label htmlFor="my-drawer-2" className="btn btn-ghost normal-case text-xldrawer-button lg:hidden">
+        <AiOutlineMenuFold className="text-2xl" />
+      </label>
+    </div>
   </>
 );
 
