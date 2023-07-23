c00c6afc7c98aa70379ded062d8aa20bf73659ed
shafiuyushawu <yishafiu@gmail.com>
2023-07-23 22:09:11 +0000

    Modified  reservations to fit mobile and desktop.

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { GiCancel } from 'react-icons/gi';
 import { useDispatch, useSelector } from 'react-redux';
 import { cancelReservation, fetchReservations } from '../redux/slices/carSlice';
 import MenuIcon from '../components/MenuIcon';
@@ -8,59 +9,81 @@ import SideBar from '../components/SideBar';
 const Reservations = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { reservations } = useSelector((state) => state.cars);
-  const user = useSelector((state) => state.user.user);
-  useEffect(() => {
-    dispatch(fetchReservations());
-  }, [dispatch]);
+  const reservations = useSelector((state) => state.cars.reservations);
+  const cars = useSelector((state) => state.cars.car);
 
-  const handleCancelReservation = (resevationId) => {
-    dispatch(cancelReservation(resevationId));
+  useEffect(() => {
+    if (reservations.length === 0) dispatch(fetchReservations());
+  }, [dispatch, reservations.length]);
+
+  const handleCancelReservation = (reservationId) => {
+    dispatch(cancelReservation(reservationId));
     navigate('/reservation', { replace: true });
   };
 
   return (
     <div className="drawer lg:drawer-open">
-      {/* eslint-disable jsx-a11y/label-has-associated-control */}
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      <div className="drawer-content md:px-5">
         <MenuIcon />
-        <h1 className="text-4xl">RESERVATIONS</h1>
         <div>
-          {reservations.length === 0 ? (
-            <p>No reservations found.</p>
-          ) : (
-            <ul>
-              {reservations.map((reservation) => (
-                <li key={reservation.id}>
-                  {/* Display reservation details here */}
-                  <p>
-                    Username:
-                    {user.username}
-                  </p>
-                  <p>
-                    Vehicle:
-                    {user.username}
-                  </p>
-                  <p>
-                    City:
-                    {reservation.city}
-                  </p>
-                  <p>
-                    Date:
-                    {reservation.reservation_date}
-                  </p>
-                  <p>
-                    City:
-                    {reservation.returned_date}
-                  </p>
-                  <button onClick={() => handleCancelReservation(reservation.id)} type="submit" className="btn btn-success">
-                    Cancel Reservation
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
+          <table className=" border table-zebra w-full">
+            <thead>
+              <tr className="mx-5 border text-black text-[14px] text-start">
+                <th className="text-start text-2xl">Car</th>
+                <th className="text-start text-2xl">Date</th>
+                <th className="text-start text-2xl">City</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservations
+                && reservations.map((reservation) => {
+                  const car = cars[reservation.car_id];
+                  return (
+                    <tr className="border my-5" key={reservation.id}>
+                      <td className="text-center">
+                        <div className="flex flex-col items-center gap-1 md:gap-3 md:flex-row">
+                          <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                              {car.photo && <img src={car.photo} alt="Avatar Tailwind CSS Component" />}
+                            </div>
+                          </div>
+                          <div>
+                            <div className="font-bold">{car.name}</div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="w-10/12 flex flex-col gap-3 justify-start">
+                        <span className="text-xl">
+                          Reserved:
+                          {' '}
+                          <p className="badge badge-ghost font-bold p-3 bg-[#99cc33]">
+                            {reservation.reservation_date}
+                          </p>
+                        </span>
+                        <span className="text-xl">
+                          Returned:
+                          {' '}
+                          <p className="badge badge-ghost font-bold p-3 bg-[#ffcc00]">
+                            {reservation.returned_date}
+                          </p>
+                        </span>
+                      </td>
+                      <td className="mb-1">{reservation.city}</td>
+                      <td>
+                        <button
+                          type="button"
+                          onClick={() => handleCancelReservation(reservation.id)}
+                          className="btn btn-sm bg-red-100 text-red-500 hover:bg-red-500 hover:text-red-100"
+                        >
+                          <GiCancel className="text-2xl" />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
         </div>
       </div>
       <SideBar />
