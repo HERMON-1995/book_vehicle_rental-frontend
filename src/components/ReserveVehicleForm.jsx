87b011aac79162dc74581cace412827ea0a67187
shafiuyushawu <yishafiu@gmail.com>
2023-07-23 22:02:10 +0000

    Rendered car field aand catch error when submitting.

--- src/components/ReserveVehicleForm.jsx
+++ src/components/ReserveVehicleForm.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { reserveVehicle } from '../redux/slices/carSlice';
 
 const ReservationPage = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const user = useSelector((state) => state.user.user) || {};
+  const car = useSelector((state) => state.cars.car[params.car_id]) || {};
+
   const [reservationDate, setReservationDate] = useState('');
   const [returnedDate, setReturnedDate] = useState('');
   const [city, setCity] = useState('');
-  // const car = useSelector((state) => state.cars.cars[0]) || {};
-  console.log(params);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const reservationData = {
@@ -22,75 +25,110 @@ const ReservationPage = () => {
       returned_date: returnedDate,
       city,
     };
-    dispatch(reserveVehicle(reservationData))
-      .then(() => {
-        // Redirect to the Reservations page after successful reservation
-        navigate('/reservations');
-      })
-      .catch((error) => {
-        console.log('Reservation failed:', error);
-      });
+    if (reservationDate === '' && returnedDate === '' && city === '') {
+      toast.warning('Kindle fill required field');
+    } else {
+      dispatch(reserveVehicle(reservationData))
+        .then(() => {
+          navigate('/reservations');
+          toast.success(`${car.name} successfully reserved.`);
+        })
+        .catch((error) => {
+          error('Reservation failed:', error);
+        });
+    }
   };
 
   return (
-    <div>
-      <div className="reservation-page">
-        {/* eslint-disable jsx-a11y/label-has-associated-control */}
-        <form onSubmit={handleSubmit} className="reservation-form">
-          <div className="form-control">
-            <label htmlFor="date">Vehicle Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder={user.username}
-              readOnly
-              required
-              disabled
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="date">Vehicle Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder={params.name}
-              readOnly
-              required
-              disabled
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="reservationDate">Date:</label>
-            <input
-              type="date"
-              id="reservationDate"
-              value={reservationDate}
-              onChange={(e) => setReservationDate(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="returnedDate">Returned Date</label>
-            <input
-              type="date"
-              id="returnedDate"
-              value={returnedDate}
-              onChange={(e) => setReturnedDate(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="city">City:</label>
-            <input
-              type="text"
-              id="city"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Book</button>
-        </form>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${car.photo})`, // Fix the image URL reference here
+      }}
+    >
+      <div className="hero-overlay bg-green-600 bg-opacity-60" />
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold">
+            Reserve
+            {' '}
+            <small className="text-4xl">{car.name}</small>
+          </h1>
+          <form onSubmit={handleSubmit} className="reservation-form">
+            {/* eslint-disable jsx-a11y/label-has-associated-control */}
+            <div className="form-control">
+              <label className="input-group input-group-vertical">
+                <span className="bg-green-800 text-white font-bold">Name</span>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder={user.username}
+                  readOnly
+                  required
+                  className="input input-bordered focus:outline-none placeholder:text-black"
+                />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="input-group input-group-vertical">
+                <span className="bg-green-800 text-white font-bold">Vehicle</span>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder={car.name}
+                  readOnly
+                  required
+                  className="input input-bordered focus:outline-none placeholder:text-black"
+                />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="input-group input-group-vertical">
+                <span className="bg-green-800 text-white font-bold flex justify-between items-center">
+                  <h3>Reservation Date</h3>
+                  <h3 className="text-red-600 text-2xl">*</h3>
+                </span>
+                <input
+                  type="date"
+                  value={reservationDate}
+                  onChange={(e) => setReservationDate(e.target.value)}
+                  className="input input-bordered text-black"
+                />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="input-group input-group-vertical">
+                <span className="bg-green-800 text-white font-bold flex justify-between items-center">
+                  <h3>Returned Date</h3>
+                  <h3 className="text-red-600 text-2xl">*</h3>
+                </span>
+                <input
+                  type="date"
+                  value={returnedDate}
+                  onChange={(e) => setReturnedDate(e.target.value)}
+                  className="input input-bordered focus:outline-none  text-black"
+                />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="input-group input-group-vertical">
+                <span className="bg-green-800 text-white font-bold flex justify-between items-center">
+                  <h3>City</h3>
+                  <h3 className="text-red-600 text-2xl">*</h3>
+                </span>
+                <input
+                  type="text"
+                  id="city"
+                  placeholder="City"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  className="input input-bordered focus:outline-none text-black"
+                />
+              </label>
+            </div>
+            <button type="submit" className="btn bg-green-800 w-full">RESERVE</button>
+          </form>
+        </div>
       </div>
     </div>
   );
